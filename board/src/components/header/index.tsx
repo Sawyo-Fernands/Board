625321698391b66eec1ecc2e93221864bb87676f
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2022-03-12 11:51:52 -0300

    Criando componente Header

---
+++ board/src/components/header/index.tsx
@@ -0,0 +1,22 @@
+import styles from './styles.module.scss'
+import Link from 'next/link'
+
+export function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+
+                <Link href={'/'}>
+                     <img src="/images/logo.svg" alt="Logo Board" />
+                </Link>
+
+                <nav> 
+                    <Link href={'/'}><a >Home</a></Link>
+                    <Link href={'/board'}><a >Meu Board</a></Link>
+                </nav>
+               
+                <button>Entrar com Github</button>
+            </div>
+        </header>
+    )
+}
