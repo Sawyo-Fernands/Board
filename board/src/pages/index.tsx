06ad52ffc07f7a42eff660ee1822f265af898eaf
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2022-03-12 13:11:05 -0300

    Criando Home

--- board/src/pages/index.tsx
+++ board/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head"
-
+import styles from '../styles/styles.module.scss'
 
 export default function Home() {
   return (
@@ -9,9 +9,23 @@ export default function Home() {
         <title>Board - Organizando suas Tarefas!</title>
     </Head>
 
-    <div >
-       <h1>Projeto</h1>
-    </div>
+    <main className={styles.contentContainer}>
+      <img src="/images/principal.svg" alt="Ferramenta board" />
+
+      <section className={styles.callToActions}>
+        <h1>Uma ferramenta para seu dia a dia Escreva, planeje e organize-se..</h1>
+        <p>
+          <span>100% Gratuito </span> e Online.
+        </p>
+      </section>
+
+      <h3 className={styles.donatersTitle}>Apoiadores:</h3>
+      <div className={styles.donaters}>
+        
+        <img src="https://sujeitoprogramador.com/steve.png" alt="" />
+       
+      </div>
+    </main>
 
     </>
    
