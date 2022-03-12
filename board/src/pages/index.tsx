5dba990cd80ac5a67e848ad4aede54d4e87627d2
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2022-03-12 11:32:31 -0300

    Adicionando estilização global

--- board/src/pages/index.tsx
+++ board/src/pages/index.tsx
@@ -1,9 +1,19 @@
+import Head from "next/head"
 
 
 export default function Home() {
   return (
+    <>
+
+    <Head>
+        <title>Board - Organizando suas Tarefas!</title>
+    </Head>
+
     <div >
-     <h1>Projeto</h1>
+       <h1>Projeto</h1>
     </div>
+
+    </>
+   
   )
 }
